0939c1ba99a1e83c09208c3bb61bfbe5682ceaa5
Jakub Ciemięga <jakub.ciemiega@gmail.com>
2021-03-21 16:44:03 +0100

    Created skeleton for the evolution loop

--- evolution_algorithm.py
+++ evolution_algorithm.py
@@ -6,9 +6,30 @@ Authors: Jakub Ciemięga, Krzysztof Piątek
 """
 
 import numpy as np
+from individual import generate_individual, create_image
 
 
-def objective_function(input_image: np.ndarray, img: np.ndarray) -> float:
+def initialize_population(pop_size: int, img_size: int) -> list:
+    """ Generate iniital population
+
+    :param pop_size: size of the population
+    :type pop_size: int
+
+    :param img_size: size of the square input image
+    :type img_size: int
+
+    :return pop: initial population pop_size individuals
+    :rtype pop: list
+    """
+
+    pop = []
+    for i in range(pop_size):
+        pop.append(generate_individual(img_size))
+
+    return pop
+
+
+def objective_function(input_image: np.ndarray, individuals: list) -> np.ndarray:
     """
     calculate value of objective function by comparing the given image with input_image and calculating average absolute
     error
@@ -16,18 +37,27 @@ def objective_function(input_image: np.ndarray, img: np.ndarray) -> float:
     :param input_image: image the algorithm is trying to achieve
     :type input_image: np.ndarray with shape (height, width, 3) where height=width
 
-    :param img: image to calculate the objective function for
-    :type img: np.ndarray: np.ndarray with shape (height, width, 3) where height=width
+    :param individuals: list of individuals to calculate the objective function for
+    :type individuals: list of np.ndarray
 
-    :return val: value of the objective function calculated for two given images
-    :rtype: float
+    :return scores: values of the objective function calculated for the given individuals
+    :rtype scores: np.ndarray of floats
     """
 
-    abs_error_b = np.sum(np.abs(input_image[:, :, 0] - img[:, :, 0]))
-    abs_error_g = np.sum(np.abs(input_image[:, :, 1] - img[:, :, 1]))
-    abs_error_r = np.sum(np.abs(input_image[:, :, 2] - img[:, :, 2]))
+    # convert input_image to to int to perform subtraction correctly (uint8 only supports 0-255 so 2-4 = 254)
+    input_image = input_image.copy().astype(int)
 
-    # average absolute error for all colors
-    val = (abs_error_b + abs_error_g + abs_error_r) / img.shape[0] ** 2
+    scores = np.zeros(len(individuals))
 
-    return val
+    for i in range(len(individuals)):
+        img = create_image(input_image.shape[0], individuals[i]).astype(int)
+
+        # calculate errors for each color channel
+        abs_error_b = np.sum(np.abs(input_image[:, :, 0] - img[:, :, 0]))
+        abs_error_g = np.sum(np.abs(input_image[:, :, 1] - img[:, :, 1]))
+        abs_error_r = np.sum(np.abs(input_image[:, :, 2] - img[:, :, 2]))
+
+        # average absolute error for all colors
+        scores[i] = (abs_error_b + abs_error_g + abs_error_r) / img.shape[0] ** 2
+
+    return scores
--- main.py
+++ main.py
@@ -10,11 +10,12 @@ Warsaw University of Technology
 
 import numpy as np
 import cv2
-from individual import generate_individual, create_image
-from evolution_algorithm import objective_function
+from individual import create_image
+from evolution_algorithm import initialize_population, objective_function
 
 img_size = 400
-pop_size = 20
+pop_size = 5
+num_generations = 300
 
 if __name__ == "__main__":
     # for tests use random seed: 300418
@@ -25,12 +26,34 @@ if __name__ == "__main__":
     cv2.imshow("Input image", input_img)
     cv2.waitKey()
 
-    pop = []
-    for i in range(pop_size):
-        pop.append(generate_individual(img_size))
-        # print(pop[i])
-        img = create_image(img_size, pop[i])
-        print(objective_function(input_img, img))
-        # cv2.imshow('Individual '+ str(i), img)
+    # create initial population and its scores
+    pop = initialize_population(pop_size, img_size)
+    scores = objective_function(input_img, pop)
 
-    cv2.waitKey()
+    # find best individual and their score
+    # noinspection PyTypeChecker
+    best_ind = pop[np.argmin(scores)]
+    best_score = np.min(scores)
+
+    cur_gen = 0
+    while cur_gen < num_generations:
+
+        # TODO implement selection_tournament() in evolution_algorithm.py
+        # pop_selection = selection_tournament(pop, scores)
+        # TODO implement mutation() in evolution_algorithm.py
+        # pop_mutation = mutation(pop_selection)
+
+        # new_scores = objective_function(input_img, pop_mutation)
+
+        # TODO implement succession_elite() in evolution_algorithm.py
+        # pop, scores = succession_elite(pop, scores, pop_mutation, new_scores)
+
+        # if np.min(scores) < best_score:
+        #     best_score = np.min(scores)
+        #     best_ind = pop[np.argmin(scores)]
+
+        if cur_gen % 100 == 0:
+            cv2.imshow('Generation ' + str(cur_gen) + ', score: ' + str(best_score), create_image(img_size, best_ind))
+            cv2.waitKey()
+
+        cur_gen += 1
